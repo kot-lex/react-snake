5134e5e6237d81c44fb51f91d3e804d95d0140c4
Aleksei Chernov <kot.lex@gmail.com>
2023-01-17 13:06:17 +0200

    Add link to github repo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import Game from './components/Game';
 
 function App() {
   return (
+    <>
+    <h1>Snake</h1>
+    <p>
+      Snake game written in react. <br />
+      Source code <a href="https://github.com/kot-lex/react-snake">https://github.com/kot-lex/react-snake</a>
+    </p>
     <Game />
+    </>
   );
 }
 
