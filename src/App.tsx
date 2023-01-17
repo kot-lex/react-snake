a390de2f8268aae7fce2b421e4229a97b670c068
Aleksei Chernov <kot.lex@gmail.com>
2023-01-17 13:12:45 +0200

    Tweak text styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import './App.css';
 import Game from './components/Game';
 
 function App() {
   return (
     <>
-    <h1>Snake</h1>
-    <p>
-      Snake game written in react. <br />
-      Source code <a href="https://github.com/kot-lex/react-snake">https://github.com/kot-lex/react-snake</a>
-    </p>
+    <div className="AppHeader">
+      <h1>Snake</h1>
+      <p>
+        Snake game written in react. <br />
+        Source code <a href="https://github.com/kot-lex/react-snake">https://github.com/kot-lex/react-snake</a>
+      </p>
+    </div>
     <Game />
     </>
   );
